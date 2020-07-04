ec5ea2cd0fb0a6938d34ad13eeeb9b787cdcc0bc
ParwizGawhari <gawhariparwiz@gmail.com>
2020-07-04 05:59:27 -0700

    practice test1

---
+++ src/main/java/us/tecschool/testNG/sdet01/testNG/sdet01/App.java
@@ -0,0 +1,13 @@
+package us.tecschool.testNG.sdet01.testNG.sdet01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/us/tecschool/testNG/sdet01/testNG/sdet01/AppTest.java
@@ -0,0 +1,20 @@
+package us.tecschool.testNG.sdet01.testNG.sdet01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
